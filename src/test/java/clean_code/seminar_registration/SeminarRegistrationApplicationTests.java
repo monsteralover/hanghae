b5e5d3b7d858540711299050ac0de77f2ef2fa38
monsteralover <kmj99301@gmail.com>
2024-12-24 00:07:42 +0900

    [feat] 프로젝트 시작

---
+++ src/test/java/clean_code/seminar_registration/SeminarRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package clean_code.seminar_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
