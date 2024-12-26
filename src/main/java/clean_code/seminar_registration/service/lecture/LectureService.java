6be335c2dc46203186f807d83d461a906dc48bc6
monsteralover <kmj99301@gmail.com>
2024-12-26 20:22:08 +0900

    Revert "[modify] LectureValidator에 과거 수강일자 불가 로직 추가"
    This reverts commit 04eace73f46ab351c6daee419a91cf0f165b48f7.

--- src/main/java/clean_code/seminar_registration/service/lecture/LectureService.java
+++ src/main/java/clean_code/seminar_registration/service/lecture/LectureService.java
@@ -1,6 +1,5 @@
 package clean_code.seminar_registration.service.lecture;
 
-import clean_code.seminar_registration.domain.lecture.Lecture;
 import clean_code.seminar_registration.repository.lecture.LectureRepository;
 import clean_code.seminar_registration.service.lecture.response.SearchLecturesResponse;
 import clean_code.seminar_registration.service.lecture.response.SearchedLecture;
@@ -17,10 +16,10 @@ public class LectureService {
     private final LectureValidator lectureValidator;
     private final LectureRepository lectureRepository;
 
-    public SearchLecturesResponse getLecturesByDate(final String date) {
-        LocalDate lectureTime = lectureValidator.validateLectureDate(date);
+    public SearchLecturesResponse getLecturesByDate(final String time) {
+        LocalDate lectureTime = lectureValidator.validateLectureTime(time);
 
-        List<Lecture> lectures =
+        List<clean_code.seminar_registration.domain.lecture.Lecture> lectures =
                 lectureRepository.getLecturesByDate(lectureTime);
         return SearchLecturesResponse.builder()
                 .lectures(lectures.stream().map(SearchedLecture::from).toList())
--- src/main/java/clean_code/seminar_registration/service/lecture/LectureValidator.java
+++ src/main/java/clean_code/seminar_registration/service/lecture/LectureValidator.java
@@ -1,7 +1,6 @@
 package clean_code.seminar_registration.service.lecture;
 
 import clean_code.seminar_registration.exception.DateFormatMalformedException;
-import clean_code.seminar_registration.exception.InvalidLectureDateException;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -9,18 +8,12 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class LectureValidator {
-    public LocalDate validateLectureDate(final String date) {
-        if (date == null || !date.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
+    public LocalDate validateLectureTime(final String time) {
+        if (time == null || !time.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
             throw new DateFormatMalformedException("날짜 형식은 'yyyy-MM-dd' 입니다.");
         }
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        final LocalDate localDate = LocalDate.parse(date, formatter);
-
-        if (localDate.isBefore(LocalDate.now())) {
-            throw new InvalidLectureDateException("과거의 일자는 수강이 불가합니다.");
-        }
-
-        return localDate;
+        return LocalDate.parse(time, formatter);
     }
 }
