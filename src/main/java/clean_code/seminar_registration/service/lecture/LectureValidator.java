e958a42d404f6336a97b9b93f7f4488f97990b37
monsteralover <kmj99301@gmail.com>
2024-12-26 20:20:43 +0900

    [modify] LectureValidator에 과거 수강일자 불가 로직 추가

--- src/main/java/clean_code/seminar_registration/service/lecture/LectureValidator.java
+++ src/main/java/clean_code/seminar_registration/service/lecture/LectureValidator.java
@@ -1,6 +1,7 @@
 package clean_code.seminar_registration.service.lecture;
 
 import clean_code.seminar_registration.exception.DateFormatMalformedException;
+import clean_code.seminar_registration.exception.InvalidLectureDateException;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -8,12 +9,18 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class LectureValidator {
-    public LocalDate validateLectureTime(final String time) {
-        if (time == null || !time.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
+    public LocalDate validateLectureDate(final String date) {
+        if (date == null || !date.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
             throw new DateFormatMalformedException("날짜 형식은 'yyyy-MM-dd' 입니다.");
         }
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        return LocalDate.parse(time, formatter);
+        final LocalDate localDate = LocalDate.parse(date, formatter);
+
+        if (localDate.isBefore(LocalDate.now())) {
+            throw new InvalidLectureDateException("과거의 일자는 수강이 불가합니다.");
+        }
+
+        return localDate;
     }
 }
